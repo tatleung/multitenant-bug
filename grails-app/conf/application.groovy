cd3374c713ffb4754ed67f1c0284dac0b8c23547
Tat M. Leung <tatml721@gmail.com>
2016-12-14 19:38:41 -0800

    enable Spring Security Core

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,29 @@
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'multitenant.bug.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'multitenant.bug.UserRole'
+grails.plugin.springsecurity.authority.className = 'multitenant.bug.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/dbconsole/**',   access: ['permitAll']],
+	[pattern: '/book/**',        access: ['ROLE_USER']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
