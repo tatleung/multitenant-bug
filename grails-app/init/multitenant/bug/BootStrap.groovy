bef9c420dae93839478ac855d35e630c40a7fd13
Tat M. Leung <tatml721@gmail.com>
2016-12-15 10:29:18 -0800

    mysql test

--- grails-app/init/multitenant/bug/BootStrap.groovy
+++ grails-app/init/multitenant/bug/BootStrap.groovy
@@ -3,16 +3,16 @@ package multitenant.bug
 class BootStrap {
 
     def init = { servletContext ->
-        // create ROLE_USER
-        def roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(true)
-
-        // create 2 test users
-        def user1 = User.findByUsername("user1") ?: new User(username: "user1", password: "password").save(true)
-        def user2 = User.findByUsername("user2") ?: new User(username: "user2", password: "password").save(true)
-
-        // assign user roles
-        UserRole.create user1, roleUser
-        UserRole.create user2, roleUser
+//        // create ROLE_USER
+//        def roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(true)
+//
+//        // create 2 test users
+//        def user1 = User.findByUsername("user1") ?: new User(username: "user1", password: "password").save(true)
+//        def user2 = User.findByUsername("user2") ?: new User(username: "user2", password: "password").save(true)
+//
+//        // assign user roles
+//        UserRole.create user1, roleUser
+//        UserRole.create user2, roleUser
     }
 
     def destroy = {
